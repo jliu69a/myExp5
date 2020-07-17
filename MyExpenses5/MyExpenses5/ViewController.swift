338e2e27c5a0ad4cb872466f20fe04d7496d7e91
jliu_home <johnsonliu69@gmail.com>
2020-07-16 20:55:38 -0400

    create initial project.

---
+++ MyExpenses5/MyExpenses5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyExpenses5
+//
+//  Created by Johnson Liu on 7/16/20.
+//  Copyright © 2020 Home Office. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
