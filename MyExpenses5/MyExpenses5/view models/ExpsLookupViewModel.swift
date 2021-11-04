d2cb2499fa1523c7363caa64fc0663ee35e7c610
Johnson Liu <liu_johnson@yahoo.com>
2021-11-04 16:26:56 -0400

    edit for expense lookup.

---
+++ MyExpenses5/MyExpenses5/view models/ExpsLookupViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ExpsLookupViewModel.swift
+//  MyExpenses5
+//
+//  Created by Johnson Liu on 11/4/21.
+//  Copyright © 2021 Home Office. All rights reserved.
+//
+
+import UIKit
+
+class ExpsLookupViewModel: NSObject {
+
+}
