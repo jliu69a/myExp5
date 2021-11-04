48934a01c64dd8b11477a33960e63d0e721ec3aa
Johnson Liu <liu_johnson@yahoo.com>
2021-11-04 17:38:00 -0400

    UI changes for the vendors lookup.

---
+++ MyExpenses5/MyExpenses5/controllers/VendorsLookupDataViewController.swift
@@ -0,0 +1,99 @@
+//
+//  VendorsLookupDataViewController.swift
+//  MyExpenses5
+//
+//  Created by Johnson Liu on 11/4/21.
+//  Copyright © 2021 Home Office. All rights reserved.
+//
+
+import UIKit
+
+class VendorsLookupDataViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var yearLabel: UILabel!
+    @IBOutlet weak var progressIndicator: UIActivityIndicatorView!
+    
+    let viewModel = VendorsLookupViewModel()
+    
+    var selectedYear: String = "0"
+    var selectedVendorId: String = "0"
+    
+    //MARK: -
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Vendors Lookup"
+        
+        viewModel.clearVendorLookupData()
+        
+        let backButton = UIBarButtonItem()
+        backButton.title = "Back"
+        self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
+        
+        self.tableView.register(UINib(nibName: "ExpenseCell", bundle: nil), forCellReuseIdentifier: "CellId")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.tableView.layer.borderColor = UIColor.systemOrange.cgColor
+        self.tableView.layer.borderWidth = 0.5
+        
+        self.yearLabel.text = String(format: "( Year: %@ )", self.selectedYear)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        progressIndicator.startAnimating()
+        showVendorsLookupData()
+    }
+    
+    func showVendorsLookupData() {
+        viewModel.vendorsLookupData(year: self.selectedYear, vendorId: self.selectedVendorId) { (any: Any) in
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+                self.progressIndicator.stopAnimating()
+            }
+        }
+    }
+    
+}
+
+extension VendorsLookupDataViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    //MARK: - table view source
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return viewModel.lookupSections()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.lookupItemForSection(section: section)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = self.tableView.dequeueReusableCell(withIdentifier: "CellId") as? ExpenseCell else {
+            return UITableViewCell()
+        }
+        
+        cell.isForLookup = true
+        
+        let expsData = viewModel.lookupData(indexPath: indexPath)
+        cell.displayModelData(data: expsData)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return viewModel.lookupTitleForSection(section: section)
+    }
+    
+    //MARK: - table view delegate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
--- MyExpenses5/MyExpenses5/controllers/VendorsLookupViewController.swift
+++ MyExpenses5/MyExpenses5/controllers/VendorsLookupViewController.swift
@@ -8,18 +8,11 @@
 
 import UIKit
 
-class VendorsLookupViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, SelectMonthAndYearViewControllerDelegate, PaymentsVendorsViewControllerDelegate {
-    
-    @IBOutlet weak var topView: UIView!
+class VendorsLookupViewController: UIViewController, SelectMonthAndYearViewControllerDelegate, PaymentsVendorsViewControllerDelegate {
     
     @IBOutlet weak var selectYearButton: UIButton!
     @IBOutlet weak var selectVendorButton: UIButton!
     @IBOutlet weak var lookupButton: UIButton!
-    
-    @IBOutlet weak var resultView: UIView!
-    @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var startNewLookupButton: UIButton!
-    @IBOutlet weak var yearLabel: UILabel!
     
     let viewModel = VendorsLookupViewModel()
     
@@ -38,17 +31,17 @@ class VendorsLookupViewController: UIViewController, UITableViewDataSource, UITa
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.showTopView()
+        self.title = "Vendors Lookup"
+        
+        let backButton = UIBarButtonItem()
+        backButton.title = "Back"
+        self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
         
         viewModel.clearVendorLookupData()
         
-        self.tableView.register(UINib(nibName: "ExpenseCell", bundle: nil), forCellReuseIdentifier: "CellId")
-        
         self.selectedYear = Date().dateToText(formate: "yyyy")
-        
         self.selectYearButton.setTitle(self.selectedYear, for: UIControl.State.normal)
         self.selectYearButton.setTitle(self.selectedYear, for: UIControl.State.highlighted)
-        self.displayYear()
         
         self.selectVendorButton.setTitle(self.selectedVendorName, for: UIControl.State.normal)
         self.selectVendorButton.setTitle(self.selectedVendorName, for: UIControl.State.highlighted)
@@ -57,41 +50,32 @@ class VendorsLookupViewController: UIViewController, UITableViewDataSource, UITa
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        self.hideResultView()
-        
-        self.tableView.layer.borderColor = UIColor.systemOrange.cgColor
-        self.tableView.layer.borderWidth = 0.5
-        
         self.selectYearButton.layer.cornerRadius = 5
         self.selectVendorButton.layer.cornerRadius = 5
         self.lookupButton.layer.cornerRadius = 5
-        self.startNewLookupButton.layer.cornerRadius = 5
     }
     
-    //MARK: - top view
-    
-    func showTopView() {
-        let frame = self.topView.frame
-        if let vc = TopBarManager.sharedInstance.createTopHeader(frame: frame, title: "Vendor Lookup", isForAdmin: false) {
-            vc.delegate = self
-            self.topView.addSubview(vc.view)
-            self.addChild(vc)
-        }
+    func showAlert(title: String?, message: String?) {
+        
+        let alert: UIAlertController = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction( UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil) )
+        self.present(alert, animated: true, completion: nil)
     }
     
     //MARK: - IB functions
     
-    @IBAction func gobackAction(_ sender: Any) {
-        self.navigationController?.popViewController(animated: true)
-    }
-    
     @IBAction func lookupAction(_ sender: Any) {
         
-        viewModel.vendorsLookupData(year: self.selectedYear, vendorId: self.selectedVendorId) { (any: Any) in
-            DispatchQueue.main.async {
-                self.showResultView()
-                self.tableView.reloadData()
-            }
+        if self.selectedVendorId == "0" {
+            showAlert(title: "Need to select a vendor.", message: nil)
+            return
+        }
+        
+        let storyboard = UIStoryboard(name: "pandv", bundle: nil)
+        if let vc = storyboard.instantiateViewController(identifier: "VendorsLookupDataViewController") as? VendorsLookupDataViewController {
+            vc.selectedYear = self.selectedYear
+            vc.selectedVendorId = self.selectedVendorId
+            self.navigationController?.pushViewController(vc, animated: true)
         }
     }
     
@@ -118,61 +102,6 @@ class VendorsLookupViewController: UIViewController, UITableViewDataSource, UITa
         }
     }
     
-    @IBAction func startNewLookupAction(_ sender: Any) {
-        
-        self.viewModel.clearVendorLookupData()
-        self.hideResultView()
-        self.tableView.setContentOffset(CGPoint.zero, animated: false)
-    }
-    
-    //MARK: - result view
-    
-    func showResultView() {
-        self.resultView.isHidden = false
-    }
-    
-    func hideResultView() {
-        self.resultView.isHidden = true
-    }
-    
-    func displayYear() {
-        self.yearLabel.text = String(format: "( Year: %@ )", self.selectedYear)
-        self.yearLabel.backgroundColor = UIColor.systemGray6
-    }
-    
-    //MARK: - table view source
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return viewModel.lookupSections()
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.lookupItemForSection(section: section)
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        guard let cell = self.tableView.dequeueReusableCell(withIdentifier: "CellId") as? ExpenseCell else {
-            return UITableViewCell()
-        }
-        
-        cell.isForLookup = true
-        
-        let expsData = viewModel.lookupData(indexPath: indexPath)
-        cell.displayModelData(data: expsData)
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return viewModel.lookupTitleForSection(section: section)
-    }
-    
-    //MARK: - table view delegate
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        self.tableView.deselectRow(at: indexPath, animated: true)
-    }
-    
     //MARK: - delegates
     
     func cancelYearSelection() {
@@ -184,7 +113,6 @@ class VendorsLookupViewController: UIViewController, UITableViewDataSource, UITa
         self.selectedYear = selectedYear
         self.selectYearButton.setTitle(self.selectedYear, for: UIControl.State.normal)
         self.selectYearButton.setTitle(self.selectedYear, for: UIControl.State.highlighted)
-        self.displayYear()
         
         self.selectVC?.dismiss(animated: true, completion: nil)
     }
@@ -199,16 +127,5 @@ class VendorsLookupViewController: UIViewController, UITableViewDataSource, UITa
             self.selectVendorButton.setTitle(displayVendor, for: UIControl.State.normal)
             self.selectVendorButton.setTitle(displayVendor, for: UIControl.State.highlighted)
         }
-    }
-}
-
-extension VendorsLookupViewController: TopHeaderViewControllerDelegate {
-    
-    func goback() {
-        self.navigationController?.popViewController(animated: true)
-    }
-    
-    func showAdmin() {
-        //
     }
 }
